3fd8fa7b0ef94aad6bc382d4fe6a9b1b97cea7c8
F0x1d <i@f0x1d.com>
2022-12-26 13:19:43 +0500

    Fixed publishing

--- utils/publish.js
+++ utils/publish.js
@@ -3,7 +3,13 @@ import request from '../utils/apiUtils'
 export default function (slug, content, accessToken, setLoading, endPoint, callback) {
     setLoading(true)
 
-    let jsonBody = { "slug": slug, "content": content, "accessToken": accessToken }
+    let jsonBody = { "content": content, "accessToken": accessToken === '' ? undefined : accessToken }
+    if (slug !== "") {
+        jsonBody = {
+            "slug": slug,
+            ...jsonBody
+        }
+    }
 
     request("https://foxbin.f0x1d.com/" + endPoint, {
         method: "POST",
